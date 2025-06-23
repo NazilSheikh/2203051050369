1b3fe185a8853951fc9383aca23d67a5de905b93
Nazil Sheikh <nazilsheikh84@gmail.com>
2025-06-23 18:28:58 +0530

    make changes in login authentication

--- URLShortnerMicroservie/models/User.js
+++ URLShortnerMicroservie/models/User.js
@@ -1,13 +1,13 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const UserSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   mobileno: String,
-//   githubUsername: String,
-//   rollno: String,
-//   collegeName: String,
-//   accessCode: String,
-// });
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  mobileno: String,
+  githubUsername: String,
+  rollno: String,
+  collegeName: String,
+  accessCode: String,
+});
 
-// module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
